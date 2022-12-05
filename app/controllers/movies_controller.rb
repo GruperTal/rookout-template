bef1f3f9eda9fbfba52d84a5ba0ed413e74f1328
Tal Gruper <tal.gruper@folloze.com>
2022-12-05 18:14:24 +0200

    added loop

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
     def index
+        numbers = [1, 3, 5, 7]
+        numbers.each do |number|
+            puts number
+        end
+
         @movies = Movie.all
         render json: @movies
     end
